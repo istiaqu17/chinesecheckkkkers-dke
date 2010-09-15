ca88a696d081984274248b58c43a1d306d93e300
sbavdhorst@gmail.com <sbavdhorst@gmail.com@2f89a61f-b508-a854-b37f-673028c6c196>
2010-09-15 09:44:07 +0000

    Modified both classes

--- Position.java
+++ Position.java
@@ -12,10 +12,12 @@ package ChineseCheckers;
 public class Position {
     private int x;
     private int y;
+    private Piece piece;
 
-    public Position(int initX, int initY){
+    public Position(int initX, int initY, Piece p){
         x = initX;
         y = initY;
+        piece = p;
     }
 
     public int getX(){
@@ -24,5 +26,24 @@ public class Position {
 
     public int getY(){
         return y;
+    }
+
+    public Piece getPiece(){
+        return piece;
+    }
+
+    public void addPiece(Piece p){
+        if(!occupied())
+            piece = p;
+        else
+            System.out.println("Already a piece on this position");
+    }
+
+    public void removePiece(){
+        piece = null;
+    }
+
+    public boolean occupied(){
+        return piece != null;
     }
 }
