6c6501a215dcd28d9b6cfdd85b1f0c80065445e1
sccvanwijk <sccvanwijk@2f89a61f-b508-a854-b37f-673028c6c196>
2010-09-16 08:53:18 +0000

    suus

---
+++ DKECheckers/src/FrameWork/Move.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package FrameWork;
+
+/**
+ *
+ * @author Suzanne
+ */
+public class Move {
+    public void test() {
+        
+    }
+}
