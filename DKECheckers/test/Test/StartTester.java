f1732503edd84a387a1c196972840ef0f424c254
satu490@hotmail.com <satu490@hotmail.com@2f89a61f-b508-a854-b37f-673028c6c196>
2010-10-06 21:55:24 +0000

    Finished GUI uploaded, uses BoardFrame (moved to FrameWork) to create correct size and layout for the container of board. Use StartTester in Test to run program.

--- DKECheckers/test/Test/StartTester.java
+++ DKECheckers/test/Test/StartTester.java
@@ -10,14 +10,17 @@ package Test;
  * @author Satu
  */
 
-import FrameWork.Start;
+
 import javax.swing.JFrame;
+import FrameWork.Start;
 
 public class StartTester {
 
-    public static void main(String[] args) {
-        JFrame startPopUp = new Start();
+    public static void main(String args[]) {
+
+        Start startPopUp = new Start();
         startPopUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         startPopUp.setVisible(true);
+
     }
 }
